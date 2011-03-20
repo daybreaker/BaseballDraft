aff4efb9fdbdda4056dba7d2ed36616649749ee1
daybreaker <daybreaker@gmail.com>
2011-03-20 10:46:22 -0500

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BaseballDraft::Application.config.secret_token = '8563e03d99a68b478249ced8e83cdeadc3bcad4b5d6dddcd764aa23af50c4344e1f3edc02c8f64e0396b47cac083dfc466ca7eeeaece6fd17d14186e45692ba9'
